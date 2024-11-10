7ba191299fca9dc471de5a60c3da06b6fbc7600f
SphereStacking <sphere.stacking@gmail.com>
2024-11-10 14:55:50 +0900

    update functions

--- .zsh/rc/functions.zsh
+++ .zsh/rc/functions.zsh
@@ -1,7 +1,9 @@
+# git log
 function glp() {
   git --no-pager log -$1
 }
 
+# git diff
 function gd() {
   if [[ -z $1 ]]; then
     git diff --color | diff-so-fancy
@@ -10,6 +12,7 @@ function gd() {
   fi
 }
 
+# git diff --cached
 function gdc() {
   if [[ -z $1 ]]; then
     git diff --color --cached | diff-so-fancy
@@ -18,18 +21,22 @@ function gdc() {
   fi
 }
 
+# myディレクトリに移動
 function i() {
   cd ~/_i/$1
 }
 
+# リポジトリディレクトリに移動
 function repros() {
   cd ~/_r/$1
 }
 
+# フォークディレクトリに移動
 function forks() {
   cd ~/_f/$1
 }
 
+# プルリクエストを操作
 function pr() {
   if [ $1 = "ls" ]; then
     gh pr list
@@ -38,10 +45,12 @@ function pr() {
   fi
 }
 
+# ディレクトリ作成
 function dir() {
   mkdir $1 && cd $1
 }
 
+# リポジトリクローン
 function clone() {
   if [[ -z $2 ]]; then
     hub clone "$@" && cd "$(basename "$1" .git)"
@@ -50,18 +59,27 @@ function clone() {
   fi
 }
 
+# インデックスディレクトリに移動してリポジトリクローン
 function clonei() {
   i && clone "$@" && $EDITOR . && cd ~2
 }
 
+# リポジトリディレクトリに移動してリポジトリクローン
 function cloner() {
   repros && clone "$@" && $EDITOR . && cd ~2
 }
 
+# フォークディレクトリに移動してリポジトリクローン
 function clonef() {
   forks && clone "$@" && $EDITOR . && cd ~2
 }
 
+# インデックスディレクトリに移動してコードエディタを開く
 function codei() {
   i && $EDITOR "$@" && cd -
+}
+
+# ディレクトリ移動時にlsを実行
+function cd() {
+  builtin cd "$1" && ls
 }
