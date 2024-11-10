030157a1c3cb4dae3995ca9ec0ae5e2a4ae94e34
SphereStacking <sphere.stacking@gmail.com>
2024-11-10 18:45:27 +0900

    update install.sh

--- .bin/install.sh
+++ .bin/install.sh
@@ -17,26 +17,44 @@ helpmsg() {
   command echo "  --install-fzf-extension"
 }
 
+backup_and_link() {
+  local src=$1
+  local dest=$2
+
+  for item in "$src"/*; do
+    local base_item=$(basename "$item")
+    local dest_item="$dest/$base_item"
+
+    if [[ -d "$item" ]]; then
+      # ディレクトリの場合、再帰的に処理
+      if [[ ! -d "$dest_item" ]]; then
+        command mkdir -p "$dest_item"
+      fi
+      backup_and_link "$item" "$dest_item"
+    else
+      # ファイルの場合、バックアップしてリンクを作成
+      if [[ -L "$dest_item" ]]; then
+        command rm -f "$dest_item"
+      fi
+      if [[ -e "$dest_item" ]]; then
+        command mv "$dest_item" "$HOME/.dotbackup"
+      fi
+      command ln -snf "$item" "$dest_item"
+    fi
+  done
+}
+
 link_to_homedir() {
   command echo "backup old dotfiles..."
-  if [ ! -d "$HOME/.dotbackup" ];then
+  if [ ! -d "$HOME/.dotbackup" ]; then
     command echo "$HOME/.dotbackup not found. Auto Make it"
     command mkdir "$HOME/.dotbackup"
   fi
 
   local script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
   local dotdir=$(dirname ${script_dir})
-  if [[ "$HOME" != "$dotdir" ]];then
-    for f in $dotdir/.??*; do
-      [[ `basename $f` == ".git" ]] && continue
-      if [[ -L "$HOME/`basename $f`" ]];then
-        command rm -f "$HOME/`basename $f`"
-      fi
-      if [[ -e "$HOME/`basename $f`" ]];then
-        command mv "$HOME/`basename $f`" "$HOME/.dotbackup"
-      fi
-      command ln -snf $f $HOME
-    done
+  if [[ "$HOME" != "$dotdir" ]]; then
+    backup_and_link "$dotdir" "$HOME"
   else
     command echo "same install src dest"
   fi
