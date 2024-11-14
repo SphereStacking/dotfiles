d6d436b8d32d1877b8d61415c0bcb74f9bd37f5c
SphereStacking <sphere.stacking@gmail.com>
2024-11-15 07:10:48 +0900

    update create symlink script

--- .bin/install.sh
+++ .bin/install.sh
@@ -3,62 +3,24 @@ set -ue
 
 # リポジトリのディレクトリを取得
 DOTFILES_DIR="$(cd "$(dirname "$0")" && pwd)"
-echo "dotfilesディレクトリ: $DOTFILES_DIR"
+command echo "dotfilesディレクトリ: $DOTFILES_DIR"
 
 source "$DOTFILES_DIR/install_homebrew.sh"
 source "$DOTFILES_DIR/install_oh_my_zsh.sh"
 source "$DOTFILES_DIR/install_fzf_extension.sh"
-
+source "$DOTFILES_DIR/install_asdf.sh"
+source "$DOTFILES_DIR/script_create_symlink_dotfiles.sh"
 
 helpmsg() {
   command echo "Usage: $0 [--help | -h]" 0>&2
-  command echo "  --install-homebrew"
-  command echo "  --install-oh-my-zsh"
-  command echo "  --install-fzf-extension"
+  command echo "  --install-homebrew|-homebrew"
+  command echo "  --install-oh-my-zsh|-ohmyzsh"
+  command echo "  --install-fzf-extension|-fzf"
+  command echo "  --install-asdf|-asdf"
+  command echo "  --install-all|-all"
+  command echo "  --create-symlink|-symlink"
 }
 
-backup_and_link() {
-  local src=$1
-  local dest=$2
-
-  for item in "$src"/*; do
-    local base_item=$(basename "$item")
-    local dest_item="$dest/$base_item"
-
-    if [[ -d "$item" ]]; then
-      # ディレクトリの場合、再帰的に処理
-      if [[ ! -d "$dest_item" ]]; then
-        command mkdir -p "$dest_item"
-      fi
-      backup_and_link "$item" "$dest_item"
-    else
-      # ファイルの場合、バックアップしてリンクを作成
-      if [[ -L "$dest_item" ]]; then
-        command rm -f "$dest_item"
-      fi
-      if [[ -e "$dest_item" ]]; then
-        command mv "$dest_item" "$HOME/.dotbackup"
-      fi
-      command ln -snf "$item" "$dest_item"
-    fi
-  done
-}
-
-link_to_homedir() {
-  command echo "backup old dotfiles..."
-  if [ ! -d "$HOME/.dotbackup" ]; then
-    command echo "$HOME/.dotbackup not found. Auto Make it"
-    command mkdir "$HOME/.dotbackup"
-  fi
-
-  local script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
-  local dotdir=$(dirname ${script_dir})
-  if [[ "$HOME" != "$dotdir" ]]; then
-    backup_and_link "$dotdir" "$HOME"
-  else
-    command echo "same install src dest"
-  fi
-}
 
 while [ $# -gt 0 ];do
   case ${1} in
@@ -78,18 +40,27 @@ while [ $# -gt 0 ];do
     --install-fzf-extension|-fzf)
       install_fzf_extension
       ;;
+    --install-asdf|-asdf)
+      install_asdf
+      ;;
     --install-all|-all)
       install_homebrew
       install_oh_my_zsh
       install_fzf_extension
+      install_asdf
+      ;;
+    --create-symlink|-symlink)
+      create_symlink_dotfiles
+      git config --global include.path "~/.gitconfig_shared"
       ;;
     *)
+      command echo "Invalid option: $1"
+      helpmsg
+      exit 1
       ;;
   esac
   shift
 done
 
-link_to_homedir
-git config --global include.path "~/.gitconfig_shared"
 command echo -e "Install completed!!!!"
 
