a52fd47e5ca7bb8f806cf5bd12c679a1e5ef5a45
SphereStacking <sphere.stacking@gmail.com>
2024-11-18 05:40:48 +0900

    asdfでnodeの最新版を追加

--- .bin/install_asdf.sh
+++ .bin/install_asdf.sh
@@ -9,4 +9,7 @@ install_asdf(){
     git clone https://github.com/asdf-vm/asdf.git ~/.asdf
     echo ". $HOME/.asdf/asdf.sh" >> ~/.zshrc
   fi
+  asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+  asdf install nodejs latest
+  asdf global nodejs latest
 }
